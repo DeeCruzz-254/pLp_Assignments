4c88fa1c6ee264a079180aa012e989ce473f5c76
DeeCruzz-254 <Samuelwagithomo@gmail.com>
2025-08-01 22:14:56 +0300

    .\calculator.py

---
+++ calculator.py
@@ -0,0 +1,9 @@
+# calculator.py
+
+# Simple calculator to add two numbers
+# This script prompts the user for two numbers and prints their sum.
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+sum = num1 + num2
+print("The sum is:", sum)
